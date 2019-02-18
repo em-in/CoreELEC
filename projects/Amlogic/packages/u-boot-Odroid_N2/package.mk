5b1cb98b67c18a16f3ce3a4f62954856b2d460c5
Ray <1458889+Raybuntu@users.noreply.github.com>
2019-02-18 14:12:38 +0100

    Add u-boot-Odroid_N2 package

---
+++ projects/Amlogic/packages/u-boot-Odroid_N2/package.mk
@@ -0,0 +1,33 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+# Copyright (C) 2019-present Team CoreELEC (https://coreelec.org)
+
+PKG_NAME="u-boot-Odroid_N2"
+PKG_VERSION="48a550ea5c66d3169111a0bdb359701f50bc23c5"
+PKG_SHA256="af95da861e6ca95d3347e4b390edd580044bdd9ca1ef1edf9e1b82d235b986ff"
+PKG_ARCH="arm aarch64"
+PKG_LICENSE="GPL"
+PKG_SITE="https://www.denx.de/wiki/U-Boot"
+PKG_URL="https://github.com/CoreELEC/u-boot/archive/$PKG_VERSION.tar.gz"
+PKG_DEPENDS_TARGET="toolchain gcc-linaro-aarch64-elf:host gcc-linaro-arm-eabi:host u-boot_firmware"
+PKG_LONGDESC="Das U-Boot is a cross-platform bootloader for embedded systems."
+PKG_TOOLCHAIN="manual"
+
+configure_package() {
+  PKG_UBOOT_CONFIG="odroidn2_defconfig"
+}
+
+pre_configure_target() {
+  cp -r $(get_build_dir u-boot_firmware)/* $PKG_BUILD
+}
+
+make_target() {
+  [ "${BUILD_WITH_DEBUG}" = "yes" ] && PKG_DEBUG=1 || PKG_DEBUG=0
+  export PATH=$TOOLCHAIN/lib/gcc-linaro-aarch64-elf/bin/:$TOOLCHAIN/lib/gcc-linaro-arm-eabi/bin/:$PATH
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make mrproper
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make $PKG_UBOOT_CONFIG
+  DEBUG=${PKG_DEBUG} CROSS_COMPILE=aarch64-elf- ARCH=arm CFLAGS="" LDFLAGS="" make HOSTCC="$HOST_CC" HOSTSTRIP="true"
+}
+
+makeinstall_target() {
+    : # nothing
+}
