6c97c57c5044b11399a4db677c9120789c9e5801
em-in <e-myn@yandex.ru>
2019-03-30 15:14:39 +0300

    mc: added from AlexELEC (part 2)

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -7,7 +7,7 @@ PKG_VERSION=""
 PKG_LICENSE="various"
 PKG_SITE="http://www.alexelec.in.ua"
 PKG_URL=""
-PKG_DEPENDS_TARGET="toolchain $PROJECT"
+PKG_DEPENDS_TARGET="toolchain"
 PKG_SECTION="virtual"
 PKG_LONGDESC="OEM: Metapackage for various OEM packages"
 
