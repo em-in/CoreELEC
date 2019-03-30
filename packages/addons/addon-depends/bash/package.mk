7f463aa4203c7b45ceb09503758ff6a4b865e999
em-in <e-myn@yandex.ru>
2019-03-30 09:05:04 +0300

    bash: update to 5.0

--- packages/addons/addon-depends/bash/package.mk
+++ packages/addons/addon-depends/bash/package.mk
@@ -2,11 +2,11 @@
 # Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="bash"
-PKG_VERSION="4.3.46"
-PKG_SHA256="fc7131bbc3139db143ec9fe03fd4e6bd3636bdf53e6956065b261f034b6af74b"
+PKG_VERSION="5.0"
+PKG_SHA256="b4a80f2ac66170b2913efbfb9f2594f1f76c7b1afd11f799e22035d63077fb4d"
 PKG_LICENSE="GPL"
 PKG_SITE="http://www.gnu.org/software/bash/bash.html"
-PKG_URL="ftp://ftp.cwru.edu/pub/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_URL="ftp://ftp.gnu.org/pub/$PKG_NAME/$PKG_NAME-$PKG_VERSION.tar.gz"
 PKG_DEPENDS_TARGET="toolchain ncurses"
 PKG_LONGDESC="The GNU Bourne Again shell."
 
