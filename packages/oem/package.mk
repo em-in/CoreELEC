3272ebe807109037db0bfc525d44fb2ef256bd34
em-in <e-myn@yandex.ru>
2019-03-30 14:55:51 +0300

    mc: added from AlexELEC

--- packages/oem/package.mk
+++ packages/oem/package.mk
@@ -1,11 +1,15 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 # Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2011-present Alex@ELEC (http://alexelec.in.ua)
 
 PKG_NAME="oem"
 PKG_VERSION=""
 PKG_LICENSE="various"
-PKG_SITE="http://www.libreelec.tv"
+PKG_SITE="http://www.alexelec.in.ua"
 PKG_URL=""
 PKG_DEPENDS_TARGET="toolchain $PROJECT"
 PKG_SECTION="virtual"
 PKG_LONGDESC="OEM: Metapackage for various OEM packages"
+
+# tools
+PKG_DEPENDS_TARGET+=" MC"
