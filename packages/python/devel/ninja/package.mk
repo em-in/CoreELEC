c94d37af78540d7108e57c3b4c573c64a31839b4
em-in <e-myn@yandex.ru>
2019-03-30 08:43:06 +0300

    ninja: update to 1.9.0

--- packages/python/devel/ninja/package.mk
+++ packages/python/devel/ninja/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
 
 PKG_NAME="ninja"
-PKG_VERSION="1.8.2"
-PKG_SHA256="86b8700c3d0880c2b44c2ff67ce42774aaf8c28cbf57725cb881569288c1c6f4"
+PKG_VERSION="1.9.0"
+PKG_SHA256="5d7ec75828f8d3fd1a0c2f31b5b0cea780cdfe1031359228c428c1a48bfcd5b9"
 PKG_LICENSE="Apache"
-PKG_SITE="http://martine.github.io/ninja/"
+PKG_SITE="https://ninja-build.org"
 PKG_URL="https://github.com/ninja-build/ninja/archive/v$PKG_VERSION.tar.gz"
 PKG_DEPENDS_HOST="Python2:host"
 PKG_LONGDESC="Small build system with a focus on speed"
